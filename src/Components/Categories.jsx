b71f376a20be7589e5d13508863827755b21bc6d
Aditya Sagar <aditya1800.be20@chitkara.edu.in>
2023-10-05 15:28:15 +0530

    first commit

---
+++ src/Components/Categories.jsx
@@ -0,0 +1,89 @@
+const Categories = [
+    {
+      id: 1,
+      title: "Men's Black Shirt",
+      price: 213,
+      category: "Men",
+      image: "https://picsum.photos/200/300?random=1",
+    },
+    {
+      id: 2,
+      title: "Women's Black Shirt",
+      price: 113,
+      category: "Women",
+      image: "https://picsum.photos/200/300?random=2",
+    },
+    {
+      id: 3,
+      title: "Children's Blue Jeans",
+      price: 45,
+      category: "Children",
+      image: "https://picsum.photos/200/300?random=3",
+    },
+    {
+      id: 4,
+      title: "Men's White T-Shirt",
+      price: 99,
+      category: "Men",
+      image: "https://picsum.photos/200/300?random=4",
+    },
+    {
+      id: 5,
+      title: "Women's Red Dress",
+      price: 189,
+      category: "Women",
+      image: "https://picsum.photos/200/300?random=5",
+    },
+    {
+      id: 6,
+      title: "Children's Sneakers",
+      price: 60,
+      category: "Children",
+      image: "https://picsum.photos/200/300?random=6",
+    },
+    {
+      id: 7,
+      title: "Men's Blue Jeans",
+      price: 79,
+      category: "Men",
+      image: "https://picsum.photos/200/300?random=7",
+    },
+    {
+      id: 8,
+      title: "Women's White Blouse",
+      price: 129,
+      category: "Women",
+      image: "https://picsum.photos/200/300?random=8",
+    },
+    {
+      id: 9,
+      title: "Children's Red Jacket",
+      price: 75,
+      category: "Children",
+      image: "https://picsum.photos/200/300?random=9",
+    },
+    {
+      id: 10,
+      title: "Men's Black Leather Shoes",
+      price: 199,
+      category: "Black",
+      image: "https://picsum.photos/200/300?random=10",
+    },
+    {
+      id: 11,
+      title: "Women's Black Dress",
+      price: 149,
+      category: "Black",
+      image: "https://picsum.photos/200/300?random=11",
+    },
+    {
+      id: 12,
+      title: "Men's White Shirt",
+      price: 159,
+      category: "White",
+      image: "https://picsum.photos/200/300?random=12",
+    },
+  ];
+  
+  export default Categories;
+  
